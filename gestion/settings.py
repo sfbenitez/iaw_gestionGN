fe4bc740f61f59fc067060a84e4eca6aa9ee80c8
root <root@mickey.sergio.gonzalonazareno.org>
2017-11-16 13:11:18 +0000

    Dumpdata without contenttypes and auth.Permission

--- gestion/settings.py
+++ gestion/settings.py
@@ -77,12 +77,24 @@ WSGI_APPLICATION = 'gestion.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+      'default': {
+          'ENGINE': 'django.db.backends.mysql',
+          'NAME': 'gestiona',
+          'USER': 'gestion',
+          'PASSWORD': 'usuario',
+          'HOST': 'localhost',
+          'PORT': '',
+      }
+  }
+
+
 
 
 # Password validation
